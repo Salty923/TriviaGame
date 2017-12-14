ddfa735fd52d59830821dcce6e29e1f4651832e0
Scott Saltzman <salty923@gmail.com>
2017-12-13 19:52:23 -0600

    add images for question responses and link js,css to html

---
+++ assets/javascript/trivia.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+
+    var questions = {
+        q1: "What is the advantage of using minimized version of jQuery?",
+        q2: "jQuery can be used in following scenarios:",
+        q3: "Which sign is used as a shortcut for jQuery?",
+        q4: "How many columns are there in the Bootstrap grid system?",
+        q5: "How many rows can you use with Bootstrap?",
+        q1: "What class with apply float-left with Bootstrap?",
+        q1: "How do you center an element horizontally using css?",
+    }
+       
+    
+console.log(questions.q1);
+
+console.log(questions.q2);
+
+});
+
+
+//var person = {
+//    firstName:"John",
+//    lastName:"Doe",
+//    age:50,
+//    eyeColor:"blue"
+//};
